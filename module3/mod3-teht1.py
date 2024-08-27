88582e7e755cbffeaee9d95a9f07fc695d1a67b7
mehdi <mehdicontacto@gmail.com>
2024-08-27 13:58:24 +0300

    Teht 3.1 korjattu

--- module3/mod3-teht1.py
+++ module3/mod3-teht1.py
@@ -3,4 +3,4 @@ pituus = float(input("Syötä kuhan pituus senttimetreinä: "))
 if pituus <= 36:
     print("Laske kuhan takaisin järveen. " + str(36 - pituus) + " cm senttiä alimmasta sallitusta pyyntimitasta puuttuu!" )
 else:
-    print("Kuhan piitus on " + pituus)
+    print("Kuhan piitus on " + str(pituus))
