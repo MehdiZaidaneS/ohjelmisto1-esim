625631f6dfcbc9948d95d1e52b1c6066a137b933
mehdi <mehdicontacto@gmail.com>
2024-08-27 14:15:51 +0300

    Tehtävä 4.6 korjattu

--- module4/mod4-teht6.py
+++ module4/mod4-teht6.py
@@ -11,7 +11,7 @@ while pisteidenmaara > 0:
     y = random.randint(-1, 1)
     pisteidenmaara = pisteidenmaara - 1
     N = N + 1
-    if x*2 + y*2 < 1:
+    if x ** 2 + y ** 2 < 1:
         n = n + 1
 
 
